d95a200eac1c90662535de6dca8738048e6c44eb
SudipKumarSah-0934 <bsse034@iit.du.ac.bd>
2024-07-03 16:16:23 +0545

    weather app commit

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # coding_test_
-
-A new Flutter project.
+Weather App
+- A simple weather app using Flutter that displays current weather information
+## Features
+ 1. Use [weatherapi](https://www.weatherapi.com/docs/) to fetch weather data.
+ 2. The app should have two screens: 
+    - Home screen with a search bar to enter a city name 
+    - Weather details screen to display the weather information
+ 3. On the home screen:
+    - Implementasearchbar where users can enter a city name
+    - Addabuttontotrigger the weather search
+    - Displayaloading indicator while fetching data
+ 4. On the weather details screen, display the following information:
+    - Cityname,Current temperature (in Celsius), Weather condition (e.g., cloudy, sunny,rainy), 
+    - An icon representing the weather condition,Humidity percentage, Wind speed
+ 5. Implemented proper error handling for API requests and displays user-friendly error messages.
+ 6. Used appropriate Getx state management to manage the app's state.
+ 7. Implemented a basic responsive design that works on both mobile and tablet devices.
+ 8. Added a "Refresh" button on the weather details screen to fetch updated weather data.
+ 9. Implemented shared prefrence for data persistence  to save the last searched city
+## Installation
+ - Clone and run directly
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,48 @@
-import 'package:coding_test_/screens/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+import 'app/data/local/my_shared_pref.dart';
+import 'app/modules/splash/bindings/splash_binding.dart';
+import 'app/routes/app_pages.dart';
+import 'config/theme/my_theme.dart';
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+void main() async {
+  // wait for bindings
+  WidgetsFlutterBinding.ensureInitialized();
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-            selectedItemColor: Color.fromARGB(255, 109, 162, 211),
-            unselectedItemColor: Colors.black),
-      ),
-      home: const HomeScreen(),
-    );
-  }
+  // init shared preference
+  await MySharedPref.init();
+
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(390, 844),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      useInheritedMediaQuery: true,
+      rebuildFactor: (old, data) => true,
+      builder: (context, widget) {
+        return GetMaterialApp(
+          title: 'Weather App',
+          useInheritedMediaQuery: true,
+          debugShowCheckedModeBanner: false,
+          builder: (context, widget) {
+            return Theme(
+              data:
+                  MyTheme.getThemeData(isLight: MySharedPref.getThemeIsLight()),
+              child: MediaQuery(
+                data: MediaQuery.of(context)
+                    .copyWith(textScaler: const TextScaler.linear(1.0)),
+                child: widget!,
+              ),
+            );
+          },
+          initialBinding: SplashBinding(),
+          initialRoute: AppPages.INITIAL,
+          getPages: AppPages.routes,
+
+        );
+      },
+    ),
+  );
 }
